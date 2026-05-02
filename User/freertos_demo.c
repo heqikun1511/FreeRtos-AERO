7ef98839412ad28b1389c43c720b8c5b1e675db3
heqikun1511 <3669308367@qq.com>
2026-05-02 20:39:44 +0800

    软件定时器

--- User/freertos_demo.c
+++ User/freertos_demo.c
@@ -3,7 +3,7 @@
 #include "./BSP/LED/led.h"
 #include "./BSP/LCD/lcd.h"
 #include "./BSP/KEY/key.h"
-#include "./SYSTEM/delay/delay.h"`
+#include "./SYSTEM/delay/delay.h"
 /*FreeRTOS*********************************************************************************************/
 #include "FreeRTOS.h"
 #include "task.h"
@@ -11,6 +11,7 @@
 #include "FreeRTOSConfig.h"
 #include "semphr.h"
 #include "event_groups.h"
+#include "timers.h"
 
 EventGroupHandle_t event_group_handler; /* 事件组句柄 */
 /******************************************************************************************************/
@@ -38,27 +39,13 @@ void start_task(void *pvParameters);        /* 任务函数 */
 #define TASK1_STK_SIZE  128                 /* 任务堆栈大小 */
 TaskHandle_t            Task1Task_Handler;  /* 任务句柄 */
 void task1(void *pvParameters);             /* 任务函数 */
-
-/* TASK2 任务 配置
- * 包括: 任务句柄 任务优先级 堆栈大小 创建任务
- */
-#define TASK2_PRIO      3                   /* 任务优先级 */
-#define TASK2_STK_SIZE  128                 /* 任务堆栈大小 */
-TaskHandle_t            Task2Task_Handler;  /* 任务句柄 */
-void task2(void *pvParameters);             /* 任务函数 */
-
-/* TASK3 任务 配置
- * 包括: 任务句柄 任务优先级 堆栈大小 创建任务
- */
+void timer1_callback(TimerHandle_t xTimer);
+void timer2_callback(TimerHandle_t xTimer);
 
 
- QueueHandle_t   key_queue;      /* 队列句柄 */
- QueueHandle_t   big_data;
- char buff[100]={"大数组drthdydutduryfuyt"};
-QueueSetHandle_t queueset_handler; /* 队列集句柄 */
-QueueHandle_t   queue1_handler; /* 队列1句柄 */
-QueueHandle_t   semp_handler; /* 队列2句柄 */
- 
+
+
+
 /******************************************************************************************************/
     
 
@@ -85,14 +72,13 @@ void freertos_demo(void)
  * @param       pvParameters : 传入参数(未用到)
  * @retval      无
  */
+TimerHandle_t timer1_handler; /* 定时器句柄 */
+TimerHandle_t timer2_handler; /* 定时器句柄 */
 void start_task(void *pvParameters)
 {
     taskENTER_CRITICAL();           /* 进入临界区 */
-    event_group_handler=xEventGroupCreate();            /* 创建事件组 */
-    if(event_group_handler!=NULL){
-        printf("event group create success\r\n");
-
-    }
+    timer1_handler = xTimerCreate("timer1", pdMS_TO_TICKS(1000), pdFALSE, (void *)1, timer1_callback); /* 创建单次定时器 */
+    timer2_handler = xTimerCreate("timer2", pdMS_TO_TICKS(1000), pdTRUE, (void *)2, timer2_callback); /* 创建周期定时器 */
     /* 创建任务1 */
     xTaskCreate((TaskFunction_t )task1,
                 (const char*    )"task1",
@@ -100,19 +86,22 @@ void start_task(void *pvParameters)
                 (void*          )NULL,
                 (UBaseType_t    )TASK1_PRIO,
                 (TaskHandle_t*  )&Task1Task_Handler);
-    /* 创建任务2 */
-    xTaskCreate((TaskFunction_t )task2,
-                (const char*    )"task2",
-                (uint16_t       )TASK2_STK_SIZE,
-                (void*          )NULL,
-                (UBaseType_t    )TASK2_PRIO,
-                (TaskHandle_t*  )&Task2Task_Handler);
-  
+    
 
     vTaskDelete(StartTask_Handler); /* 删除开始任务 */
     taskEXIT_CRITICAL();            /* 退出临界区 */
 }
-
+void timer1_callback(TimerHandle_t xTimer)
+{
+    static uint32_t timer=0;
+    printf("timer1_callback%lu\r\n",++timer);
+}
+void timer2_callback(TimerHandle_t xTimer)
+{
+    static uint32_t timer2=0;
+    printf("timer2_callback%lu\r\n",++timer2);
+    
+}
 /**
  * @brief       task1
  * @param       pvParameters : 传入参数(未用到)
@@ -131,46 +120,18 @@ void task1(void *pvParameters)
         key=key_scan(0);
         if(key==KEY0_PRES)
         {
-            printf("KEY0_PRES\r\n");
-            xEventGroupSetBits(event_group_handler,0x01); /* 设置事件组的第0位 */
+            xTimerStart(timer1_handler,portMAX_DELAY); /* 启动定时器1 */
+            xTimerStart(timer2_handler,portMAX_DELAY); /* 启动定时器2 */
         }
         else if(key==KEY1_PRES)
         {
-            printf("KEY1_PRES\r\n");
-            xEventGroupSetBits(event_group_handler,0x02); /* 设置事件组的第1位 */
-        }
-        else if(key==KEY2_PRES)
-        {
-            printf("KEY2_PRES\r\n");
-            xEventGroupSetBits(event_group_handler,0x04); /* 设置事件组的第2位 */
+            xTimerStop(timer1_handler,portMAX_DELAY); /* 停止定时器1 */
+            xTimerStop(timer2_handler,portMAX_DELAY); /* 停止定时器2 */
+       
         }
         vTaskDelay(10);
-    
-    
-
-    vTaskDelay(10);
-
-
-       
-
     }
 }
+    
 
-/**
- * @brief       task2
- * @param       pvParameters : 传入参数(未用到)
- * @retval      无
- */
-void task2(void *pvParameters)
-{
-    (void)pvParameters;
-    uint8_t key=0;
-    QueueSetHandle_t memory_handler;
-    EventBits_t evenbit=0;
-    while(1)
-    {
-        memory_handler=xEventGroupWaitBits(event_group_handler,0x01||0x02||0x04,pdTRUE,pdTRUE,portMAX_DELAY); /* 等待事件组的第0、1、2位任意一个被置位 */
-        printf("the position of the bit is %d\r\n",memory_handler);
-    }
-}
-
+    
